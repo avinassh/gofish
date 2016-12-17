bf49f917a47530a17acb1737198af81d4de11d4d
avi <hi@avi.im>
2016-12-17 10:32:16 +0530

    Add main file

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fish := `       o                 o
+                  o
+         o   ______      o
+           _/  (   \_
+ _       _/  (       \_  O
+| \_   _/  (   (    0  \
+|== \_/  (   (          |
+|=== _ (   (   (        |
+|==_/ \_ (   (          |
+|_/     \_ (   (    \__/
+          \_ (      _/
+            |  |___/
+           /__/
+
+BLU BLU BLU`
+
+	fmt.Println(fish)
+}
